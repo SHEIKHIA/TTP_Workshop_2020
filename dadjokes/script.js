b87828314dbf548f74dc7ab9916aa1adf44ef006
Sheikh Ahmed <67445725+SHEIKHIA@users.noreply.github.com>
2022-01-13 16:48:37 -0500

    frontend file

---
+++ dadjokes/script.js
@@ -0,0 +1,18 @@
+const jokeE1 =document.getElementById('joke')
+const jokenB8 = document.getElementById('jokeBtn')
+jokeBtn.addEventListener('click',generateJoke)
+
+generateJoke()
+
+async function generateJoke() {
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        },
+    }
+    const res = await fetch('https://icanhazdadjoke.com',config)
+
+    const data = await res.json()
+
+    jokeE1.innerHTML=data.joke
+}
---
+++ expense_tracker/outline.md
@@ -0,0 +1,10 @@
+## Expense Tracker
+keep track of income and expenses. Add and remove items and save to local storage
+
+## project Specifications
+-Create UI for project
+-Display transation items in DOM
+-Show balance, expense and income totals
+-Add new transation and reflect in total
+-Delete items from DOM
+-Persist to local storage
